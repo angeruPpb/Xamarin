1c624e885c21fc32dda52555c3bdf05b009a255f
angeruPpb <107784067+angeruPpb@users.noreply.github.com>
2022-12-09 08:43:11 -0500

    Add files via upload

---
+++ confe/Curriculum.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static Xamarin.Essentials.Permissions;
+
+namespace confe
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Curriculum : ContentPage
+	{
+		public Curriculum (string name, string ocupation, string birthday, string email, 
+						string phone, string nacionalidad, string level, string perfil)
+		{
+			InitializeComponent ();
+            F_nombre.Text = name;
+            F_ocupacion.Text = ocupation;
+            F_edad.Text = "|| " + birthday + " || " + name;
+			F_email.Text = "E-mail: " + email;
+			F_telefono.Text = "Telefono: " + phone;
+			F_pais.Text = "Nacionalidad: " + nacionalidad;
+			F_level.Text = "Ingles: " + level;
+			F_perfil.Text = perfil;
+        }
+
+        private void btnregresar_Clicked(object sender, EventArgs e)
+        {
+			Navigation.PopAsync();
+        }
+    }
+}
---
+++ confe/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace confe
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Btniniciar_Clicked(object sender, EventArgs e)
+        {
+            string name = Name.Text;
+            string ocupation = Ocupacion.Text;
+            string birthday = Birthday.Text;
+            string email = Email.Text;
+            string phone = Phone.Text;
+            string nacionalidad = Nacionalidad.Text;
+            string level = Level.Text;
+            string perfil = Perfil.Text;
+            Navigation.PushAsync(new Curriculum(name, ocupation, birthday, email, phone, 
+                                                nacionalidad, level, perfil));
+        }
+    }
+}
